175407a43d3b07cf1d37b6a2ac1614b7c788665f
Lucas Hayes <lhaye005@gmail.odu.edu>
2023-07-06 18:03:41 -0400

    added builder file

--- app/src/main/java/edu/odu/cs/cs350/HTMLDocument.java
+++ app/src/main/java/edu/odu/cs/cs350/HTMLDocument.java
@@ -1,33 +1,5 @@
 package edu.odu.cs.cs350;
 
-import java.util.Collection;
-
 public class HTMLDocument {
-
-    public static void HTMLDocument() {
-
-    }
-
-    public static void withContentFrom(StringBuffer reader) {
-
-    }
-
-    public static void withContentFrom(String file) {
-
-    }
-
-    public static void withBaseDirectory(String[] siteRoot) {
-
-    }
-
-
-    public static void withBaseURLs(Collection<?> URLs) {
-
-    }
-
-
-    public static void extractAnchors(Collection<?> Anchors) {
-
-    }
-
+    
 }
---
+++ app/src/test/java/edu/odu/cs/cs350/HTMLDocumentBuilderTest.java
@@ -0,0 +1,11 @@
+package edu.odu.cs.cs350;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HTMLDocumentBuilderTest {
+
+    @Test HTMLDocumentBuilder() {
+        fail("Not yet implemented");
+    }
+    
+}
