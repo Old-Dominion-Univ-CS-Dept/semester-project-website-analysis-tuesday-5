f03940740a84dd62ac2dff1501080b65690c6f44
Jon Rossy <112786969+jross015@users.noreply.github.com>
2023-07-06 22:35:24 -0400

    Update WebsiteBuilder.java

--- app/src/main/java/edu/odu/cs/cs350/WebsiteBuilder.java
+++ app/src/main/java/edu/odu/cs/cs350/WebsiteBuilder.java
@@ -3,5 +3,22 @@ package edu.odu.cs.cs350;
 import java.util.Collection;
 
 public class WebsiteBuilder {
+
+public static void withPath(path: Path)
+    {
+        
+    }
+
+public static void withURL(url: URL)
+    {
+        
+    }
+
+public static void withURLs(urls: Collection<URL>)
+    {
+
+    }
+
+    
     
 }
