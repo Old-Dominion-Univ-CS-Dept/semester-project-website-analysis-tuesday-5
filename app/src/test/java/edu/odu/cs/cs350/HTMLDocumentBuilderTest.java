e5f794ea18cc8ed198252bc6eb660605fbbfb11f
Richard Connor <rconn004@odu.edu>
2023-07-09 15:59:48 -0400

    TEST1

--- app/src/test/java/edu/odu/cs/cs350/HTMLDocumentBuilderTest.java
+++ app/src/test/java/edu/odu/cs/cs350/HTMLDocumentBuilderTest.java
@@ -2,6 +2,8 @@ package edu.odu.cs.cs350;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -18,6 +20,32 @@ public class HTMLDocumentBuilderTest {
     }
 
     @BeforeEach
-    public
+    public void setUp() {
+        linkValidator = new LinkValidator();
+        visitedLinks = new HashSet<>();
+    }
+
+    @Test
+    public void testValidateLink() {
+        String link1 = "https://example.com/link1";
+        String link2 = "https://example.com/link2";
+        String link3 = "https://example.com/link3";
+
+        // Validate the first link
+        boolean isValid1 = linkValidator.validateLink(link1, visitedLinks);
+        Assertions.assertTrue(isValid1);
+        Assertions.assertTrue(visitedLinks.contains(link1));
+
+        // Validate the second link
+        boolean isValid2 = linkValidator.validateLink(link2, visitedLinks);
+        Assertions.assertTrue(isValid2);
+        Assertions.assertTrue(visitedLinks.contains(link2));
+
+        // Validate the third link (which has been visited before)
+        visitedLinks.add(link3);
+        boolean isValid3 = linkValidator.validateLink(link3, visitedLinks);
+        Assertions.assertFalse(isValid3);
+    }
+
 
 }
