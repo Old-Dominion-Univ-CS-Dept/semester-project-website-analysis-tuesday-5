50d4c365b4ad3d858ae748c52eab4134260351a7
Richard Connor <rconn004@odu.edu>
2023-07-10 22:16:58 -0400

    added code to the HTMLDocumentBuilder

--- app/src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
+++ app/src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
@@ -1,7 +1,13 @@
 package edu.odu.cs.cs350;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -53,5 +59,47 @@ public class HTMLDocumentBuilder {
     public static void build(Document HTMLDocument) {
 
     }
+
+    public static void validateLink(File htmlFile){
+        try {
+            Document document = Jsoup.parse(htmlFile, "UTF-8");
+
+            //Select all <a> tags with href attribute
+            document.select("a[href]");
+            for (Element link : links){
+                String url = link.attr("href");
+
+                if (visitedLinks.contains(url)){
+                    System.out.println(url + " has already been visited.");
+                } else{
+                    visitedLinks.add(url);
+                    if (validateLink(url)) {
+                        System.out.println(url + " is a valid link.");
+                    } else {
+                         System.out.println(url + " is not a valid link.");
+                    }
+                }
+
+            }
+
+        } 
+        catch (IOException e){
+            System.out.println("Error reading HTML file: " + e.getMessage());
+        }  
+        }
+         public static boolean validateLink(String url) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+         } catch (Exception e){
+            return false;
+         }
+
+    }
+
     
 }
