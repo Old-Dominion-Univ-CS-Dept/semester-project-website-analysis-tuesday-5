2d51578c5670d22d436ed475c0d307848706efe6
Lucas Hayes <lhaye005@gmail.odu.edu>
2023-07-11 21:04:10 -0400

    Conform to Javadoc syntax

--- app/src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
+++ app/src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
@@ -6,14 +6,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-/*
+/**
  * This is an HTML parser that takes URLs or files and extracts various tags (anchors,
  * images, stylesheets, JavaScript code) from each webpage. After extracting all of that
  * information, it will build and return an HTML Document.
  */
 public class HTMLDocumentBuilder {
 
-    /*
+    /**
      * Parse HTML from a StringBuffer
      * 
      * @param StringBuffer HTML code
@@ -45,7 +45,7 @@ public class HTMLDocumentBuilder {
 
     }
 
-    /*
+    /**
      * Extracts all anchors from a given HTML Document
      * 
      * @param an HTML Document
