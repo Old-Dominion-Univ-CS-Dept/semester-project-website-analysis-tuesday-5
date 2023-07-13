02e9deda415d304596b9dad475576b94330dd185
Richard Connor <rconn004@odu.edu>
2023-07-13 13:24:25 -0400

    took my code off of HTMLDOCBUILDER

--- app/src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
+++ app/src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
@@ -96,46 +96,6 @@ public class HTMLDocumentBuilder {
 
     }
 
-    public static void validateLink(File htmlFile){
-        try {
-            Document document = Jsoup.parse(htmlFile, "UTF-8");
-
-            //Select all <a> tags with href attribute
-            document.select("a[href]");
-            for (Element link : links){
-                String url = link.attr("href");
-
-                if (visitedLinks.contains(url)){
-                    System.out.println(url + " has already been visited.");
-                } else{
-                    visitedLinks.add(url);
-                    if (validateLink(url)) {
-                        System.out.println(url + " is a valid link.");
-                    } else {
-                         System.out.println(url + " is not a valid link.");
-                    }
-                }
-
-            }
-
-        } 
-        catch (IOException e){
-            System.out.println("Error reading HTML file: " + e.getMessage());
-        }  
-        }
-         public static boolean validateLink(String url) {
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("HEAD");
-            connection.connect();
-
-            int responseCode = connection.getResponseCode();
-            return responseCode == HttpURLConnection.HTTP_OK;
-         } catch (Exception e){
-            return false;
-         }
-
-    }
 
     
 }
