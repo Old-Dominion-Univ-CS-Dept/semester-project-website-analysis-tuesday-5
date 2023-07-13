c2370dff7341eeeedb61b20869c92e49d9aff59d
Richard Connor <rconn004@odu.edu>
2023-07-13 14:39:35 -0400

    ADDED SOME CODE TO THE WebAnalysis.java

--- app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
+++ app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
@@ -1,12 +1,30 @@
 package edu.odu.cs.cs350;
 
 public class WebAnalysis {
+        private String link;
+         // Constructor
+         public WebAnalysis(String link) {
+            this.link = link;
+        }
+            //Getter
+        public String getName() {
+            return link;
+        }
+
     public static void main(String[] args) {
         //driver -> wb: new()
         //return
+        // Create a new WebAnalysis object
+        WebAnalysis link = new WebAnalysis("John Doe");
+        // Access the object's properties
+        String name = link.getName();
+        System.out.println("Name: " + name);
+
+
 
         //driver -> wb: withPath(path)
         //return
+
 
         //driver -> withURLs(urls)
         //return
