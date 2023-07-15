07ac7987eb20493ef305122fec11d6d93393b818
Jon Rossy <112786969+jross015@users.noreply.github.com>
2023-07-14 22:29:44 -0400

    Update WebsiteBuilder.java

--- app/src/main/java/edu/odu/cs/cs350/WebsiteBuilder.java
+++ app/src/main/java/edu/odu/cs/cs350/WebsiteBuilder.java
@@ -16,15 +16,13 @@ import java.nio.file.Path;
 
 /**
  * This is a Website Parser that takes a path to a website and either/or a single website url or a collection of website urls and obtains the website path and website url from each webpage. After obtaining all of that
- * information, it will build and return a website.
+ * information, it will build and return a website including its path and url.
  */
 
 
 public class WebsiteBuilder {
 
-    /**
-    * obtains website path
-    */
+    // obtains website path
     
 public static Path withPath(String Path){
 
@@ -32,9 +30,7 @@ public static Path withPath(String Path){
     return path;
 }
    
-/**
-* obtains website URL
-*/
+// obtains website URL
     
 public static Document withURL(String URLs){
         
@@ -43,10 +39,8 @@ public static Document withURL(String URLs){
         
     }
     
-/**
-* obtains collection of website URLs
-*/
-    
+// obtains collection of website URLs
+
 public static Vector<Collection> withURLs(string URL){
         
     Vector<Collection> collect = new Vector<>();
@@ -56,10 +50,8 @@ public static Vector<Collection> withURLs(string URL){
 
 }
 
-    /**
-    * builds website with path and url
-    */
-
+    // builds website with path and url
+    
 public static void build(Website) {
 
     }
