5d72735278608d02e5a5a283ce26fc1fe9cfa5d7
Richard Connor <rconn004@odu.edu>
2023-07-14 20:44:09 -0400

    test commit for WebAnalysis

--- app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
+++ app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
@@ -1,6 +1,15 @@
 package edu.odu.cs.cs350;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Test;
 
 
 public class WebAnalysisTest {
     
+
+    @Test
+    public void path_exists() throws IOException {
+       Path link = ( "app/src/test/java/edu/odu/cs/cs350/SampleHTMLFile.html");
+       assertThat(Files.exists(link)).isTrue;
+    }
 }
