4c08c03e37b8e98f7235bf49153c12387c4bf40b
Lucas Hayes <lhaye005@gmail.odu.edu>
2023-07-14 21:20:18 -0400

    WIP

--- app/src/main/java/edu/odu/cs/cs350/HTMLDocument.java
+++ app/src/main/java/edu/odu/cs/cs350/HTMLDocument.java
@@ -1,5 +1,20 @@
 package edu.odu.cs.cs350;
 
+import java.util.ArrayList;
+import org.jsoup.nodes.Element;
+
 public class HTMLDocument {
-    
+    ArrayList<Element> anchors;
+ 
+    public HTMLDocument(ArrayList<Element> extractedAnchors) {
+        anchors = extractedAnchors;
+    }
+
+    public void setAnchors(ArrayList<Element> anchorList) {
+        anchors = anchorList;
+    }
+
+    public ArrayList<Element> getAnchors() {
+        return anchors;
+    }
 }
--- app/src/test/java/edu/odu/cs/cs350/HTMLDocumentTest.java
+++ app/src/test/java/edu/odu/cs/cs350/HTMLDocumentTest.java
@@ -1,11 +1,26 @@
 package edu.odu.cs.cs350;
 
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HTMLDocumentTest {
 
     @Test void testHTMLDocument() {
-        fail("Not yet implemented");
+        Element testElement = new Element("Thursdays are just baby Fridays");
+        ArrayList<Element> testArrayList = new ArrayList<Element>();
+        testArrayList.add(testElement);
+
+        HTMLDocument constructorHTMLDocument = new HTMLDocument(testArrayList);
+
+        assertEquals(Integer.valueOf(constructorHTMLDocument.getAnchors().size()), 1);
+
     }
+
+    
     
 }
+
