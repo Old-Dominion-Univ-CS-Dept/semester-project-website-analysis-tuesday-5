758a3bda142fd27d67adf94a3669a17a0a208416
Jon Rossy <112786969+jross015@users.noreply.github.com>
2023-07-14 22:26:37 -0400

    Update Website.java

--- app/src/main/java/edu/odu/cs/cs350/Website.java
+++ app/src/main/java/edu/odu/cs/cs350/Website.java
@@ -1,11 +1,45 @@
 package edu.odu.cs.cs350;
 
 import java.util.Collection;
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.util.Vector;
+import java.nio.file.Path;
+
 
 public class Website {
 
-urls: Collection<?>;
+Vector<Collection> URL;
+String Path;
 
-localDirectory: Path;
+public Website(Vector<Collection> Url, String path) {
+    URL = Url;
+    Path = path;
+}
+
+public void setPath(String path) {
+    Path = path;
+}
+
+public static Path getPath() {
+    return Path;
+}
+
+
+public void setUrl(Vector<Document> ) {
+    URL = Url;
+}
+
+public static URL getURL() {
+    return URL;
+}
     
 }
