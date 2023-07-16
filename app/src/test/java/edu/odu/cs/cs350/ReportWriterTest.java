e548ab71d42edad916e95dd420768f698d198c3e
selai01 <snaim001@odu.edu>
2023-07-15 22:29:39 -0400

    ReportWriterTest

--- app/src/test/java/edu/odu/cs/cs350/ReportWriterTest.java
+++ app/src/test/java/edu/odu/cs/cs350/ReportWriterTest.java
@@ -1,9 +1,45 @@
 package edu.odu.cs.cs350;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReportWriterTest {
-    @Test void ReportWriter() {
-        fail("Not yet implemented");
+    @Test
+    public void testJsonExtension() {
+        String extension = "json";
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String expectedFileName = now.format(formatter) + "-summary." + extension;
+
+        String fileName = ReportWriter.nameFile(extension);
+
+        Assertions.assertEquals(expectedFileName, fileName);
+    }
+
+    @Test
+    public void testTxtExtension() {
+        String extension = "txt";
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String expectedFileName = now.format(formatter) + "-summary." + extension;
+
+        String fileName = ReportWriter.nameFile(extension);
+
+        Assertions.assertEquals(expectedFileName, fileName);
+    }
+
+    @Test
+    public void testXlsxExtension() {
+        String extension = "xlsx";
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String expectedFileName = now.format(formatter) + "-summary." + extension;
+
+        String fileName = ReportWriter.nameFile(extension);
+
+        Assertions.assertEquals(expectedFileName, fileName);
     }
 }
