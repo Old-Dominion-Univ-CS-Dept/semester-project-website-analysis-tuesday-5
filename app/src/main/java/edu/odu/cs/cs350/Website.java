732de1711f97556aa42d7c114600a2f3943b5092
Jon Rossy <112786969+jross015@users.noreply.github.com>
2023-07-15 21:58:17 -0400

    Update Website.java

--- app/src/main/java/edu/odu/cs/cs350/Website.java
+++ app/src/main/java/edu/odu/cs/cs350/Website.java
@@ -14,17 +14,28 @@ import org.jsoup.select.Elements;
 import java.util.Vector;
 import java.nio.file.Path;
 
-
+/** 
+ * Website class also used for Website builder
+ */
 public class Website {
 
 Vector<Element> URL;
 static String Path;
 
+    /**
+ * Function for website URL and path
+ * @param Url
+ * @param path
+ */
 public Website(Vector<Element> Url, String path) {
     URL = Url;
     Path = path;
 }
 
+    /**
+ * Sets website path
+ * @param path
+ */
 public void setPath(String path) {
     Path = path;
 }
@@ -33,11 +44,19 @@ public void setPath(String path) {
  ///   return Path;
 ///}
 
-
+    /**
+ * Sets website url
+ * @param Url
+ */
 public void setUrl(Vector<Element> Url) {
     URL = Url;
 }
 
+    /**
+ * Gets website url
+ * @param Url
+ * @return
+ */
 public static URL getURL(java.net.URL Url) {
     return Url;
 }
