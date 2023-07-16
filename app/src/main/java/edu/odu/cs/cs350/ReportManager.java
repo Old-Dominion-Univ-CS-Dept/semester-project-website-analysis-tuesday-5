a622bc6a49dc9dc74b400261169f9bae3a9dd1db
Justin Cantoria <jcant005@odu.edu>
2023-07-15 20:00:29 -0400

    updated ReportManager

--- app/src/main/java/edu/odu/cs/cs350/ReportManager.java
+++ app/src/main/java/edu/odu/cs/cs350/ReportManager.java
@@ -3,14 +3,16 @@ package edu.odu.cs.cs350;
 
 public class ReportManager {
     
-public static void generateReport(String reportName) {
+public static String generateReport(String reportName) {
 
         System.out.println("Generating the report: " + reportName);
+        return "Generated report: " + reportName;
     }
     
-    public static void exportReport(String reportName, String format) {
+    public static String exportReport(String reportName, String format) {
 
         System.out.println("Exporting the report: " + reportName + " in format: " + format);
+        return "Exported report: " + reportName + " in " + format + " format ";
     }
 }
 
