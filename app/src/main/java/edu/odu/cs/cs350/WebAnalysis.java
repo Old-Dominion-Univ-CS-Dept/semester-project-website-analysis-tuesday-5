f430713e47c7ffde220fd96bc0bb494af56b2a3f
Richard Connor <rconn004@odu.edu>
2023-07-19 18:27:53 -0400

    made changes to WebAnalysis and WebAnalysisTest

--- app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
+++ app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
@@ -1,34 +1,56 @@
 package edu.odu.cs.cs350;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.nio.file.Path;
-import edu.odu.cs.cs350.WebsiteBuilder;
+import java.io.IOException;
 
 public class WebAnalysis extends WebsiteBuilder {
-    public static void main(String[] args) {
-       //driver -> wb: new()
-      //return 
-        WebsiteBuilder path = new WebsiteBuilder();
-            return;
-            try{
-                //Connenct to the web page and get the HTML document
-                Document doc = Jsoup.connect(URL).get();
+    public static void main(String[] args) throws IOException{
+        Validate.isTrue(args.length == 1, "usage: supply url to fetch");
+        String url = args[0];
+        print("Fetching %s...", url);
 
-                //Print the page title 
-                String title = doc.title();
-                System.out.println("Page title: " + title);
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+        Elements media = doc.select("[src]");
+        Elements imports = doc.select("link[href]");
 
-                // Print all the links in the page
-                System.out.println("Links: ");
-                Element links = doc.select("a[href]");
-                for(Element link : links){
-                    String href = link.attr("href");
-                    System.out.println(href);
-                }
-            }
-            
+        print("\nMedia: (%d)", media.size());
+        for (Element src : media) {
+            if (src.normalName().equals("img"))
+                print(" * %s: <%s> %sx%s (%s)",
+                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+                        trim(src.attr("alt"), 20));
+            else
+                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+        }
+
+        print("\nImports: (%d)", imports.size());
+        for (Element link : imports) {
+            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
+        }
+
+        print("\nLinks: (%d)", links.size());
+        for (Element link : links) {
+            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+        }
+    }
+
+    private static void print(String msg, Object... args) {
+        System.out.println(String.format(msg, args));
+    }
+
+    private static String trim(String s, int width) {
+        if (s.length() > width)
+            return s.substring(0, width-1) + ".";
+        else
+            return s;
+    }
+            //driver -> wb: new()
+           //return 
+        
             //driver -> wb: withPath(path)
             //return
       
@@ -36,4 +58,3 @@ public class WebAnalysis extends WebsiteBuilder {
             //return
     }
    //driver -> wb: build()
-}
--- app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
+++ app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
@@ -1,4 +1,6 @@
 package edu.odu.cs.cs350;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.jsoup.Jsoup;
@@ -9,14 +11,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebAnalysisTest {
     @Test
-    @void GetPageTitle(){
-        String html = "<html><head><title>Example Page Title</title></head><body></body></html>";
-        Document doc = Jsoup.parse(html);
-
-        String title = WebAnalysis.GetPageTitle(doc);
-        assertEquals("Page Title", title);
+    public void testMainMethod() throws IOException {
+        // Redirect System.out to capture the output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        
+        // Prepare the test input
+        String url = "https://example.com";
+        String[] args = { url };
+        
+        // Call the main method
+        WebAnalysis.main(args);
+        
+        // Restore System.out
+        System.setOut(System.out);
+        
+        // Retrieve the captured output
+        String output = outputStream.toString().trim();
+        
+        // Define the expected output
+        String expectedOutput = "Expected output goes here...";
+        
+        // Perform assertions
+        assertEquals(expectedOutput, output);
     }
 }
+
     
 
     
