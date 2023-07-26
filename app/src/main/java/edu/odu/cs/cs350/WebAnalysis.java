295c7f2442ae10d8047ef99a4945e23acbda9dd7
Richard Connor <rconn004@odu.edu>
2023-07-25 23:00:06 -0400

    still working on WebAnalysis

--- app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
+++ app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
@@ -1,43 +1,65 @@
 package edu.odu.cs.cs350;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
-//This is WebAnalysis a driver class for WB (WebsiteBuilder) where it will fetch the path and URL 
+//This is WebAnalysis the driver class for WB (WebsiteBuilder) where it will fetch the path and URL 
 //of the given directory
 public class WebAnalysis  {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
 
-        Path pathToAnalysis = Paths.get(args[0]);
-        System.out.println(pathToAnalysis);
+        //Will handle user arguments 
+        String webPath = args[0];
+
+        List<String> urls = Arrays.stream(args)
+            .skip(1)
+            .collect()
+            .toList();
+
+        Website site = new WebsiteBuilder();
+            .withPath()
+            .withURLs(urls)
+            .build();
+
+            ReportManager manager = new ReportManager();
+            manager.setSourceData(site);
+            manager.determineBaseFilename();
+            manager.writeAll();
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+            manager.writeReportNames(writer);
+
+        
 
         //driver -> wb: new()
         //return
         //The method withPath(String) in the type WebsiteBuilder is not applicable for the arguments ()
         //The constructor WebsiteBuilder(Path) is undefined
-        WebsiteBuilder WB = new WebsiteBuilder(pathToAnalysis);
-        WB.withPath();
+       // WebsiteBuilder WB = new WebsiteBuilder(pathToAnalysis);
+        //WB.withPath();
 
              //driver -> wb: withPath(path)
             //return
-        System.out.println("Path Identifed: ");
-        for (Path file : WB.withPath()){
-            System.out.format("  -%s%n", file);
-        }
-        System.out.println();
+        //System.out.println("Path Identifed: ");
+        //for (Path file : WB.withPath()){
+           // System.out.format("  -%s%n", file);
+       // }
+       // System.out.println();
 
             //driver -> wb: withURLs(urls)
             //return
             //The method withURL(String) in the type WebsiteBuilder is not applicable for the arguments ()
-        System.out.println("URLs Identified: ");
-        for (Path file : WB.withURLs()){
-            System.out.format("  - %s%n", file);
-        }
+       // System.out.println("URLs Identified: ");
+        //for (Path file : WB.withURLs()){
+      //      System.out.format("  - %s%n", file);
+       // }
 
         //driver -> wb: build()
-        Stream.Builder<String> builder = Stream.builder();
-        Stream<String> str = builder.build();
-        str.forEach(System.out::println);
+       // Stream.Builder<String> builder = Stream.builder();
+        //Stream<String> str = builder.build();
+       // str.forEach(System.out::println);
     }
 }    
     
--- app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
+++ app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
@@ -5,37 +5,42 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.jsoup.Jsoup;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class WebAnalysisTest {
-    @Test
-    public void testMainMethod() throws IOException {
-        // Redirect System.out to capture the output
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-        
-        // Prepare the test input
-        String url = "https://example.com";
-        String[] args = { url };
-        
-        // Call the main method
-        WebAnalysis.main(args);
-        
-        // Restore System.out
-        System.setOut(System.out);
-        
-        // Retrieve the captured output
-        String output = outputStream.toString().trim();
-        
-        // Define the expected output
-        String expectedOutput = "Expected output goes here...";
-        
-        // Perform assertions
-        assertEquals(expectedOutput, output);
+    private ReportManager manager;
+    private Website site;
+
+     @Before
+    public void setUp() {
+        manager = new ReportManager();
+        site = new Website(); // Assuming you have a testable SiteData object.
     }
+
+    @Test
+    public void testDetermineBaseFilename() {
+        manager.setSourceData(site);
+        String expectedBaseFilename = "expected_base_filename"; // Provide the expected base filename here.
+        String actualBaseFilename = manager.determineBaseFilename();
+        assertEquals(expectedBaseFilename, actualBaseFilename);
+    }
+
+    @Test
+    public void testWriteAll() {
+        manager.setSourceData(site);
+        // Assuming the writeAll() method has some side effects (e.g., writes to a file or modifies data).
+        // In this case, you may need to check the behavior and state changes rather than a direct return value.
+        // For example, you could assert that a file was created or data was updated correctly.
+        // For demonstration purposes, let's assume the writeAll() method returns a boolean indicating success.
+        boolean writeSuccess = manager.writeAll();
+        assertTrue(writeSuccess);
+    }
+    
 }
 
     
