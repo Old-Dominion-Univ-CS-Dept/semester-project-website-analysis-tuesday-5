0e1309e3b54c413a6e1ff8ff6102de9235f0c59e
snaim001 <123999922+snaim001@users.noreply.github.com>
2023-07-25 22:58:02 -0400

    Update TextReportWriterTest.java

--- app/src/test/java/edu/odu/cs/cs350/TextReportWriterTest.java
+++ app/src/test/java/edu/odu/cs/cs350/TextReportWriterTest.java
@@ -1,29 +1,76 @@
+package edu.odu.cs.cs350;
 
-    package edu.odu.cs.cs350;
+import edu.odu.cs.cs350.HTMLDocument;
+import edu.odu.cs.cs350.OtherFile;
+import edu.odu.cs.cs350.Website;
+import edu.odu.cs.cs350.enums.FileType;
+import org.junit.jupiter.api.Test;
 
-    import org.junit.Test;
-    import java.io.BufferedReader;
-    import java.io.FileReader;
-    import java.io.IOException;
-    import static org.junit.Assert.*;
-    
-    public class TextReportWriterTest {
-    
-        @Test
-        public void testWriteReport() throws IOException {
-            
-            String fileName = "test_report.txt";
-            String expectedContent = "Text report content.";
-    
-           
-            TextReportWriter.writeReport(fileName);
-    
-            
-            try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-                String actualContent = reader.readLine();
-                assertEquals(expectedContent, actualContent);
-            }
-        }
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TextReportWriterTest {
+    @Test
+    public void testWrite() {
+        // Mock the necessary data
+        Website website = mock(Website.class);
+        List<HTMLDocument> pages = createMockHTMLDocuments();
+        List<OtherFile> otherFiles = createMockOtherFiles();
+        when(website.getPages()).thenReturn(pages);
+        when(website.getOtherFiles()).thenReturn(otherFiles);
+
+        TextReportWriter reportWriter = new TextReportWriter();
+        reportWriter.setSourceData(website);
+        reportWriter.setBaseName("website");
+
+        // Call the write method
+        reportWriter.write();
+
+        // Perform assertions or verify the generated text output as needed
     }
-       
 
+    private List<HTMLDocument> createMockHTMLDocuments() {
+        List<HTMLDocument> pages = new ArrayList<>();
+
+        HTMLDocument page1 = createMockHTMLDocument("index.html", 1024);
+        HTMLDocument page2 = createMockHTMLDocument("about.html", 2048);
+
+        pages.add(page1);
+        pages.add(page2);
+
+        return pages;
+    }
+
+    private HTMLDocument createMockHTMLDocument(String fileName, long fileSize) {
+        HTMLDocument page = mock(HTMLDocument.class);
+        when(page.getFileName()).thenReturn(String.valueOf(Path.of(fileName)));
+        when(page.getFileSize()).thenReturn(fileSize);
+        return page;
+    }
+
+    private List<OtherFile> createMockOtherFiles() {
+        List<OtherFile> otherFiles = new ArrayList<>();
+
+        OtherFile imageFile = createMockOtherFile("image.png", FileType.IMAGE, 512);
+        OtherFile otherFile1 = createMockOtherFile("file1.txt", FileType.UNCATEGORIZED, 256);
+        OtherFile otherFile2 = createMockOtherFile("file2.txt", FileType.UNCATEGORIZED, 128);
+
+        otherFiles.add(imageFile);
+        otherFiles.add(otherFile1);
+        otherFiles.add(otherFile2);
+
+        return otherFiles;
+    }
+
+    private OtherFile createMockOtherFile(String fileName, FileType fileType, long fileSize) {
+        OtherFile otherFile = mock(OtherFile.class);
+        when(otherFile.getPath()).thenReturn(String.valueOf(Path.of(fileName)));
+        when(otherFile.getFileType()).thenReturn(fileType);
+        when(otherFile.getFileSize()).thenReturn(fileSize);
+        return otherFile;
+    }
+}
