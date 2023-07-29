87db10bcef09b4c02580496cfd3cff65f83dae51
Richard Connor <rconn004@odu.edu>
2023-07-28 23:37:04 -0400

    missed one

--- app/src/main/java/edu/odu/cs/cs350/ExcelReportWriter.java
+++ app/src/main/java/edu/odu/cs/cs350/ExcelReportWriter.java
@@ -3,15 +3,13 @@ package edu.odu.cs.cs350;
 public class ExcelReportWriter {
 
     public void setSourceData(Website website) {
-        // TODO
+        
     }
 
     public void setBaseName(String baseFilename) {
-        // TODO
     }
 
-    public void write() {
-        // TODO
+    public void write() {   
     }
     
 }
--- app/src/main/java/edu/odu/cs/cs350/JSONReportWriter.java
+++ app/src/main/java/edu/odu/cs/cs350/JSONReportWriter.java
@@ -3,14 +3,14 @@ package edu.odu.cs.cs350;
 public class JSONReportWriter {
 
     public void setSourceData(Website website) {
-        // TODO
+        
     }
     public void setBaseName(String baseFilename) {
-        // TODO
+        
     }
 
     public void write() {
-        // TODO
+        
     }
     
 }
