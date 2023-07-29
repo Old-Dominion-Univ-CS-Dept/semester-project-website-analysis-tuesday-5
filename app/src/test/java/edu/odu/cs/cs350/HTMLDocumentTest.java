8c3d8a6ec122e06d320b37c96b91e8d78021cc0f
Lucas Hayes <lhaye005@gmail.odu.edu>
2023-07-29 17:44:43 -0400

    Link Categorization

--- app/src/test/java/edu/odu/cs/cs350/HTMLDocumentTest.java
+++ app/src/test/java/edu/odu/cs/cs350/HTMLDocumentTest.java
@@ -2,10 +2,15 @@ package edu.odu.cs.cs350;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.jsoup.nodes.Element;
 import org.junit.Test;
+
+import edu.odu.cs.cs350.enums.Locality;
 
 
 public class HTMLDocumentTest {
@@ -27,6 +32,23 @@ public class HTMLDocumentTest {
 
         assertEquals(testElement,constructorHTMLDocument.getAnchors().get(0).getContent());
 
+    }
+
+    @Test
+    public void testAnchorCategorization() throws IOException {
+        HTMLDocumentBuilder HTMLDocBuilder = new HTMLDocumentBuilder();
+        Path pathToTestSite = Paths.get("src/test/java/edu/odu/cs/cs350/baseDir");
+        HTMLDocBuilder.withBaseDirectory(pathToTestSite);
+        HTMLDocBuilder.withContentFrom("src/test/java/edu/odu/cs/cs350/baseDir/site/testSite.html");
+        HTMLDocBuilder.extractAnchors();
+        //HTMLDocBuilder.withPathToDoc(pathToTestSite);
+        HTMLDocument HTMLDoc = HTMLDocBuilder.build();
+
+        HTMLDoc.categorizeAnchors();
+        assertEquals(3, HTMLDoc.getAnchors().size());
+        assertEquals(Locality.INTRAPAGE, HTMLDoc.getAnchors().get(0).getLocation());
+        assertEquals(Locality.EXTERNAL, HTMLDoc.getAnchors().get(1).getLocation());
+        assertEquals(Locality.INTERNAL, HTMLDoc.getAnchors().get(2).getLocation());
     }
 
     
