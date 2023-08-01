b9e28fa88b0b03e389c43c55e466b7c37ad16a41
Richard Connor <rconn004@odu.edu>
2023-07-31 21:36:22 -0400

    Added code to WebAnalysis WebAnalysisTest WebsiteBuilder

--- app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
+++ app/src/main/java/edu/odu/cs/cs350/WebAnalysis.java
@@ -1,17 +1,18 @@
 package edu.odu.cs.cs350;
-
 import edu.odu.cs.cs350.HTMLDocumentBuilder;
 import edu.odu.cs.cs350.WebsiteBuilder;
 import edu.odu.cs.cs350.HTMLDocument;
 import edu.odu.cs.cs350.OtherFile;
 import edu.odu.cs.cs350.Website;
 import edu.odu.cs.cs350.ReportManager;
+import java.nio.file.Paths;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,12 +20,18 @@ import java.util.Scanner;
 public class WebAnalysis {
     public static void main(String[] args) throws IOException {
         WebsiteBuilder wb = new WebsiteBuilder();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the path to the website directory: ");
-        Path path = Path.of(scanner.nextLine());
+       // Scanner scanner = new Scanner(System.in);
+       // System.out.println("Enter the path to the website directory: ");
+      //Path  path = Path.of(scanner.nextLine());
 
-
+        //wb.withPath(path)
+         // Create a Path object representing the website directory
+        Path path = Paths.get("path/to/website/directory");
+         // Call the withPath method to set the path in WebsiteBuilder
         wb.withPath(path);
+         // Now, let's retrieve and print the path from WebsiteBuilder
+        Path savedPath = wb.getPath();
+        System.out.println("Enter the path to the website directory: " + savedPath);
 
         Website site = wb.build();
         site.setLocalDirectory(path);
@@ -41,7 +48,7 @@ public class WebAnalysis {
                 hb.withPathToDoc(htmlFile);
                 Document doc = Jsoup.parse(htmlFile.toFile(), "UTF-8");
                // Extract resources from HTML content
-                hb.extractContent(doc);
+                hb.extractContent();
                 hb.withContentFrom(htmlFile.getFileName().toString());
                 // Build the HTMLDocument
                 // Add the HTMLDocument to the Website
@@ -53,11 +60,24 @@ public class WebAnalysis {
             }
         }
 //        wb.walkDirectoryOtherFile()
-        ReportManager rm = new ReportManager();
-        rm.setSourceData(site);
-        rm.determineBaseFilename();
+       // ReportManager rm = new ReportManager();
+       // rm.setSourceData(site);
+       // rm.determineBaseFilename();
 
-        rm.writeAll();
+       // rm.writeAll();
+        this.webPages = new ArrayList<>();
+        this.directories = new ArrayList<>();
 
-    }
+        Files.walk(siteRoot)
+            .forEach((Path path) -> {
+                if (Files.isRegularFile(path)) {
+                    this.webPages.add(path);
+                }
+                else if (Files.isDirectory(path)) {
+                    this.directories.add(path);
+                }
+            }
+            );
+
+        }
 }
--- app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
+++ app/src/test/java/edu/odu/cs/cs350/WebAnalysisTest.java
@@ -1,17 +1,31 @@
 package edu.odu.cs.cs350;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.*;
 
 
 public class WebAnalysisTest {
-	
-	@Test
+    
+
+    private WebsiteBuilder wb;
+
+    @BeforeEach 
+    void setUp(){
+        wb = new WebsiteBuilder();
+
+    }
+    
+    @Test
     void testWithPathInput() throws IOException {
         // Mock the user input
         String inputPath = "path/to/website/directory";
@@ -19,4 +33,51 @@ public class WebAnalysisTest {
         System.setIn(inputStream);
 
     }
+
+    @Test
+    void testBuild(){
+        Website site = wb.build();
+        assertEquals(Website.class, site.getLocalDirectory());
+
+    }
+
+    @Test
+    void testWithPath(){
+        Path path = Path.of("path/to/website/directory");
+        wb.withPath(path);
+        assertEquals(path, wb.getPath());
+    }
+
+    @Test
+    void testWalkDirectory() throws IOException {
+        Path path = Path.of("path/to/website/directory");
+        wb.withPath(path);
+        // Mock the DirectoryStream<Path> returned by Files.newDirectoryStream
+        DirectoryStream<Path> directoryStream = mock(DirectoryStream.class);
+        when(Files.newDirectoryStream(path)).thenReturn(directoryStream);
+        when(directoryStream.iterator()).thenReturn(Arrays.asList(Path.of("file1.html"), Path.of("file2.txt")).iterator());
+
+        List<Path> result = wb.walkDirectory(path);
+
+        assertEquals(2, result.size());
+        assertEquals(Path.of("file1.html"), result.get(0));
+        assertEquals(Path.of("file2.txt"), result.get(1));
+    }
+
+    @Test
+    void testRemoveNonHTMLFiles() {
+        List<Path> files = Arrays.asList(
+                Path.of("file1.html"),
+                Path.of("file2.txt"),
+                Path.of("file3.html")
+        );
+
+        List<Path> result = wb.removeNonHTMLFiles(files);
+
+        assertEquals(2, result.size());
+        assertEquals(Path.of("file1.html"), result.get(0));
+        assertEquals(Path.of("file3.html"), result.get(1));
+    }
+
+
 }
