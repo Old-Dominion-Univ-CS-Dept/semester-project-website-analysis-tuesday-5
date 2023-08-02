a441bb09fde285c140083eba37ff13e2b38a184c
Jon Rossy <112786969+jross015@users.noreply.github.com>
2023-08-02 18:21:06 -0400

    Update HTMLDocument.java

--- app/src/main/java/edu/odu/cs/cs350/HTMLDocument.java
+++ app/src/main/java/edu/odu/cs/cs350/HTMLDocument.java
@@ -11,6 +11,10 @@ import org.jsoup.nodes.Document;
 
 import edu.odu.cs.cs350.enums.Locality;
 
+/** 
+* HTML document class, works in conjunction with HTMLDocumentBuilder
+*/
+
 public class HTMLDocument {
     ArrayList<Anchor> anchors;
     ArrayList<Image> images;
@@ -27,33 +31,70 @@ public class HTMLDocument {
 
     String fileName;
 
+    /**
+    * Get website url function
+    @return URL
+    */
+
     public String getURL() {
         return URL;
     }
+
+    /**
+    * Sets website url function
+    @param URL
+    */
 
     public void setURL(String URL) {
         this.URL = URL;
     }
 
+    /**
+    *Gets File Name function
+    @return fileName
+    */
+
     public String getFileName() {
         return fileName;
     }
+
+    /**
+    * Sets File Name function
+    @param fileName
+    */
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
+    /**
+    * Gets File Size function
+    @param fileSize
+    */
+
     public long getFileSize() {
         return fileSize;
     }
+
+    /** Sets File size function
+    @param fileSize
+    */
 
     public void setFileSize(long fileSize) {
         this.fileSize = fileSize;
     }
 
+    /** get Resources function
+    @param resources
+    */
+
     public List<Resource> getResources() {
         return resources;
     }
+
+    /** set Resources function
+    @param resources
+    */
 
     public void setResources(List<Resource> resources) {
         this.resources = resources;
@@ -83,9 +124,19 @@ public class HTMLDocument {
         anchors = extractedAnchors;
     }
 
+    /**
+    *Set Path to Document function
+    @param path
+    */
+
     public void setPathToDocument(Path path) {
         this.pathToDoc = path;
     }
+
+    /**
+    * set Base directory function
+    @param path
+    */
 
     public void setBaseDir(Path path) {
         this.baseDir = path;
@@ -116,6 +167,12 @@ public class HTMLDocument {
         return anchors;
     }
 
+    /**
+    * Set Images function
+    @param HTMLDoc
+    @param imageList
+    */
+
     public void setImages(HTMLDocument HTMLDoc, ArrayList<Image> imageList) {
         for(Image image: imageList) {
             image.addFoundOn(HTMLDoc);
@@ -123,9 +180,18 @@ public class HTMLDocument {
         }
     }
 
+    /** get Images function
+    @return images
+    */
+
     public ArrayList<Image> getImages() {
         return images;
     }
+
+    /** set Scripts function
+    @param HTMLDoc
+    @param scriptList
+    */
 
     public void setScripts(HTMLDocument HTMLDoc, ArrayList<Script> scriptList) {
         for(Script script: scriptList) {
@@ -134,9 +200,18 @@ public class HTMLDocument {
         }
 }
 
+    /** get Scripts function
+    @param scripts
+    */
+
     public ArrayList<Script> getScripts() {
         return scripts;
     }
+
+    /** set StyleSheets function
+    @param HTMLDoc
+    @param stylesheetList
+    */
 
     public void setStyleSheets(HTMLDocument HTMLDoc, ArrayList<StyleSheet> stylesheetList) {
         for(StyleSheet stylesheet: stylesheetList) {
@@ -145,14 +220,26 @@ public class HTMLDocument {
     }
 }
 
+    /**
+    * get Stylesheets function
+    @return stylesheets
+    */
+
     public ArrayList<StyleSheet> getstylesheets() {
         return stylesheets;
     }
 
+    /** Set HTML Content function
+    * Param HTMLContent
+    */
 
     public void setHTMLContent(Document HTMLContent) {
         this.HTMLContent = HTMLContent;
     }
+
+    /** Get HTML Content function
+    @return this.HTMLContent
+    */
 
     public Document getHTMLContent() {
         return this.HTMLContent;
